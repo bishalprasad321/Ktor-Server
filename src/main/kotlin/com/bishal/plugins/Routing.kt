7b2f4d07d411931eb7f7946d14d06a14d8699d39
bishalprasad321 <bishalprasad321@gmail.com>
2023-03-18 13:30:26 +0530

    Search functionality added.

--- src/main/kotlin/com/bishal/plugins/Routing.kt
+++ src/main/kotlin/com/bishal/plugins/Routing.kt
@@ -2,6 +2,7 @@ package com.bishal.plugins
 
 import com.bishal.routes.getAllHeroes
 import com.bishal.routes.root
+import com.bishal.routes.searchHeroes
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
@@ -10,6 +11,7 @@ fun Application.configureRouting() {
     routing {
         root()
         getAllHeroes()
+        searchHeroes()
 
         static("/images") {
             resources("images")

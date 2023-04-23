58fae225eda224a46ec7809810c77774a0c13f0b
bishalprasad321 <bishalprasad321@gmail.com>
2023-04-23 22:49:06 +0530

    Server Testing for applicationTest.

--- src/test/kotlin/com/bishal/ApplicationTest.kt
+++ src/test/kotlin/com/bishal/ApplicationTest.kt
@@ -1,18 +1,20 @@
 package com.bishal
 
 import com.bishal.models.ApiResponse
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.server.testing.*
-import kotlin.test.*
-import io.ktor.http.*
 import com.bishal.plugins.*
 import com.bishal.repository.HeroRepository
+import com.bishal.repository.NEXT_PAGE_KEY
+import com.bishal.repository.PREV_PAGE_KEY
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.testing.*
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import org.koin.java.KoinJavaComponent.inject
+import kotlin.test.*
 
 
 @Suppress("DEPRECATION")
@@ -35,6 +37,62 @@ class ApplicationTest {
                 actual = bodyAsText()
             )
         }
+    }
+
+    @Test
+    fun `access all heroes endpoint, query all pages, assert correct information`() {
+        withTestApplication(moduleFunction = Application::module) {
+            val pages = 1..5
+            val heroes = listOf(
+                heroRepository.page1,
+                heroRepository.page2,
+                heroRepository.page3,
+                heroRepository.page4,
+                heroRepository.page5
+            )
+
+            pages.forEach { page ->
+                handleRequest(HttpMethod.Get, "/boruto/heroes?page=$page").apply {
+                    assertEquals(
+                        expected = HttpStatusCode.OK,
+                        actual = response.status()
+                    )
+                    val expected = ApiResponse(
+                        success = true,
+                        message = "ok",
+                        prevPage = calculatePage(page = page)["prevPage"],
+                        nextPage = calculatePage(page = page)["nextPage"],
+                        heroes = heroes[page - 1]
+                    )
+
+                    val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+                    assertEquals(
+                        expected = expected,
+                        actual = actual
+                    )
+                }
+            }
+
+        }
+    }
+
+    private fun calculatePage(page: Int) : Map<String, Int?> {
+        var prevPage: Int? = page
+        var nextPage: Int? = page
+        if (page in 1..4){
+            nextPage = nextPage?.plus(1)
+        }
+        if (page in 2..5){
+            prevPage = prevPage?.minus(1)
+        }
+        if (page == 1){
+            prevPage = null
+        }
+        if (page == 5){
+            nextPage = null
+        }
+        return mapOf(PREV_PAGE_KEY to prevPage, NEXT_PAGE_KEY to nextPage)
+
     }
 
     @ExperimentalSerializationApi
